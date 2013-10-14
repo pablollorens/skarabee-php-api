0d01eba7b9d65a3f26e4a558fc3f9ae268c4824f
Siesqo <jeroen@siesqo.be>
2013-10-14 14:21:06 +0200

    recast parameters

--- src/Skarabee/Skarabee.php
+++ src/Skarabee/Skarabee.php
@@ -58,19 +58,19 @@ class Skarabee
 		$parameters = array();
 
 		// build parameters
-		$parameters['Comments'] = $item['comments'];
-		if(isset($item['publication_id'])) $parameters['PublicationID'] = $item['publication_id'];
-		if(isset($item['external_reference'])) $parameters['ExternalReference'] = $item['external_reference'];
-		if(isset($item['first_name'])) $parameters['FirstName'] = $item['first_name'];
-		if(isset($item['last_name'])) $parameters['LastName'] = $item['last_name'];
-		if(isset($item['cell_phone'])) $parameters['CellPhone'] = $item['cell_phone'];
-		if(isset($item['phone'])) $parameters['Phone'] = $item['phone'];
-		if(isset($item['email'])) $parameters['Email'] = $item['email'];
-		if(isset($item['street'])) $parameters['Street'] = $item['street'];
-		if(isset($item['house_number'])) $parameters['HouseNumber'] = $item['house_number'];
-		if(isset($item['house_number_extension'])) $parameters['HouseNumberExtension'] = $item['house_number_extension'];
-		if(isset($item['zip'])) $parameters['ZipCode'] = $item['zip'];
-		if(isset($item['city'])) $parameters['City'] = $item['city'];
+		$parameters['Comments'] = (string) $item['comments'];
+		if(isset($item['publication_id'])) $parameters['PublicationID'] = (string) $item['publication_id'];
+		if(isset($item['external_reference'])) $parameters['ExternalReference'] = (string) $item['external_reference'];
+		if(isset($item['first_name'])) $parameters['FirstName'] = (string) $item['first_name'];
+		if(isset($item['last_name'])) $parameters['LastName'] = (string) $item['last_name'];
+		if(isset($item['cell_phone'])) $parameters['CellPhone'] = (string) $item['cell_phone'];
+		if(isset($item['phone'])) $parameters['Phone'] = (string) $item['phone'];
+		if(isset($item['email'])) $parameters['Email'] = (string) $item['email'];
+		if(isset($item['street'])) $parameters['Street'] = (string) $item['street'];
+		if(isset($item['house_number'])) $parameters['HouseNumber'] = (string) $item['house_number'];
+		if(isset($item['house_number_extension'])) $parameters['HouseNumberExtension'] = (string) $item['house_number_extension'];
+		if(isset($item['zip'])) $parameters['ZipCode'] = (string) $item['zip'];
+		if(isset($item['city'])) $parameters['City'] = (string) $item['city'];
 
 		// return call
 		return $this->doCall('InsertContactMes', $parameters);
